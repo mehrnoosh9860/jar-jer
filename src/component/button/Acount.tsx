c4b4ae4587f2f52bc9c01709859ec176e94fb15d
mehrnoosh9860 <mehrnoosh9860payan@gmail.com>
2024-06-08 03:21:06 -0700

    acount button

---
+++ src/component/button/Acount.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import "/src/app/style.css";
+
+function Acount() {
+  return (
+    <button type="button" className="inline-flex bg-[#323232] items-center mx-7 backgroundfancy text-white p-3 ">
+
+      account
+    </button>
+  )
+}
+
+export default Acount
