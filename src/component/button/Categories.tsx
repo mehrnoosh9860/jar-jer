e0cfc1d87733a68a7cea4b661bd73af1234504ac
mehrnoosh9860 <mehrnoosh9860payan@gmail.com>
2024-06-08 03:20:32 -0700

    categories component button

---
+++ src/component/button/Categories.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Categories() {
+  return (
+    <button type="button" className="inline-flex items-center mx-7">
+    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M3 6H21" stroke="#323232" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
+<path d="M17 12H3" stroke="#323232" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
+<path d="M3 18H13" stroke="#323232" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
+</svg>
+      category
+    </button>
+  )
+}
+
+export default Categories
